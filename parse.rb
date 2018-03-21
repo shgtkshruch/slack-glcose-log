adafacd03a2283e9599cc4c2509b36edc3e84db2
shgtkshruch <shgtk.shruch@gmail.com>
2018-03-21 10:24:21 +0900

    Slackのデータから必要な情報を抜き出す

---
+++ parse.rb
@@ -0,0 +1,20 @@
+require 'json'
+require 'pry-byebug'
+
+res = []
+
+Dir.glob('./glucose/*').each do |file|
+  open(file) { |io| JSON.load io }.each.with_index(0) do |object, i|
+    if object['type'] == 'message'
+      item = {
+        timestamp: object.dig('file', 'created'),
+        title: object.dig('file', 'title'),
+        image: object.dig('file', 'thumb_1024'),
+      }
+      res.push item
+    end
+  end
+end
+
+binding.pry
+p 'end'
